82cdc8608c8fc6a1981296702f898706d2fc1933
goriachevalex <goriachevalex@yandex-team.ru>
2024-06-02 17:19:35 +0400

    5 test's added

--- Chantal/ChantalUITests/ChantalUITests.swift
+++ Chantal/ChantalUITests/ChantalUITests.swift
@@ -23,12 +23,98 @@ final class ChantalUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testAddTaskToTodoList() throws {
+        
         let app = XCUIApplication()
+        
+        //Шаг 1: Перейти на Главную
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //Шаг 2: Тап по “+”
+        app.navigationBars.buttons["Add"].tap()
+        //открылся алерт для ввода задачи
+        XCTAssertTrue(app.alerts.textFields["Enter task name..."].exists)
+        //Шаг 3: ввести название задачи
+        app.alerts.textFields["Enter task name..."].tap()
+        app.alerts.textFields["Enter task name..."].typeText("New task")
+        //Шаг 4: тап по кнопке добавления в алерте
+        app.alerts.buttons["Add"].tap()
+        //алерт ввода задачи закрылся
+        XCTAssertFalse(app.alerts.element.exists)
+        //введенная задача отображается в списке
+        XCTAssertTrue(app.cells.staticTexts["New task"].exists)
+    }
+    
+    func testMarkTaskAsDone() throws {
+        
+        let app = XCUIApplication()
+        
+        //подготовка + шаг 1
+        app.launch()
+        app.navigationBars.buttons["Add"].tap()
+        app.alerts.textFields["Enter task name..."].tap()
+        app.alerts.textFields["Enter task name..."].typeText("New task")
+        app.alerts.buttons["Add"].tap()
+        //Шаг 2: Свайпнуть задачу слева направо
+        app.tables.staticTexts["New task"].swipeRight()
+        app.tables.buttons["done"].tap()
+        //проверяем что таска перешла в Done - не нашёл как проверить
+        
+    }
+    
+    func testDeleteTaskFromTodoList() throws {
+        
+        let app = XCUIApplication()
+        
+        //подготовка + шаг 1
+        app.launch()
+        app.navigationBars.buttons["Add"].tap()
+        app.alerts.textFields["Enter task name..."].tap()
+        app.alerts.textFields["Enter task name..."].typeText("New task")
+        app.alerts.buttons["Add"].tap()
+        //Шаг 2: Свайпнуть задачу справа налево
+        app.tables.staticTexts["New task"].swipeLeft()
+        app.tables.buttons["delete"].tap()
+        //проверяем что таска удалена
+        XCTAssertFalse(app.cells.staticTexts["New task"].exists)
+        
+    }
+    
+    func testDeleteTaskFromDoneList() throws {
+        
+        let app = XCUIApplication()
+        
+        //подготовка + шаг 1
+        app.launch()
+        app.navigationBars.buttons["Add"].tap()
+        app.alerts.textFields["Enter task name..."].tap()
+        app.alerts.textFields["Enter task name..."].typeText("New task")
+        app.alerts.buttons["Add"].tap()
+        app.tables.staticTexts["New task"].swipeRight()
+        app.tables.buttons["done"].tap()
+        //Шаг 2: Свайпнуть задачу справа налево
+        app.tables.staticTexts["New task"].swipeLeft()
+        app.tables.buttons["delete"].tap()
+        //проверяем что таска удалена
+        XCTAssertFalse(app.cells.staticTexts["New task"].exists)
+    }
+    
+    func testMarkDoneTaskAsDone() throws {
+        
+        let app = XCUIApplication()
+        
+        //подготовка + шаг 1
+        app.launch()
+        app.navigationBars.buttons["Add"].tap()
+        app.alerts.textFields["Enter task name..."].tap()
+        app.alerts.textFields["Enter task name..."].typeText("New task")
+        app.alerts.buttons["Add"].tap()
+        app.tables.staticTexts["New task"].swipeRight()
+        app.tables.buttons["done"].tap()
+        //Шаг 2: Свайпнуть задачу слева направо
+        app.tables.staticTexts["New task"].swipeRight()
+        // проверяем что таска всё ещё есть
+        XCTAssertTrue(app.cells.staticTexts["New task"].exists)
+        
     }
 
     func testLaunchPerformance() throws {
